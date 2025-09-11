eebabe87c2aede1b94515789d3a212dbbcd8d712
Dilna K Vasavan <dilna@xminds.com>
2025-09-11 13:39:40 +0530

    Dom element ref

--- src/app/counter/counter.ts
+++ src/app/counter/counter.ts
@@ -9,7 +9,14 @@ import { Component } from '@angular/core';
 export class Counter {
   count: number = 0;
   message = 'Click here to increase count'
-  increment() {
+  incrementFromParent() {
     this.count++;
+  }
+  increment(input: HTMLInputElement) {
+    // read directly from the DOM input
+    this.count = Number(input.value) + 1;
+
+    // update the DOM input value manually
+    input.value = this.count.toString();
   }
 }
