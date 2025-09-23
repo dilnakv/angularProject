bfd8773a226e52f1e0ba561daeafc88a30f2fa70
Dilna K Vasavan <dilna@xminds.com>
2025-09-23 13:05:52 +0530

    @HostBinding

--- src/app/customDirectives/hover-effect.ts
+++ src/app/customDirectives/hover-effect.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appHoverEffect]',
@@ -6,18 +6,31 @@ import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
 })
 export class HoverEffect {
 
-  constructor(private el: ElementRef, private renderer: Renderer2) {}
+  // constructor(private el: ElementRef, private renderer: Renderer2) {}
+  @HostBinding('style.transform') transform = 'scale(1)';
+  @HostBinding('style.backgroundColor') bgColor = 'white';
+  @HostBinding('style.boxShadow') boxShadow = '';
 
   @HostListener('mouseenter') onMouseEnter() {
-    this.renderer.setStyle(this.el.nativeElement, 'transform', 'scale(1.1)');
-    this.renderer.setStyle(this.el.nativeElement, 'backgroundColor', '#f0f8ff');
-    this.renderer.setStyle(this.el.nativeElement, 'boxShadow', '0 4px 12px rgba(0,0,0,0.2)');
+    console.log("MOUSR");
+    
+    // this.renderer.setStyle(this.el.nativeElement, 'transform', 'scale(1.1)');
+    // this.renderer.setStyle(this.el.nativeElement, 'backgroundColor', '#f0f8ff');
+    // this.renderer.setStyle(this.el.nativeElement, 'boxShadow', '0 4px 12px rgba(0,0,0,0.2)');
+
+    this.transform = 'scale(1.1)';
+    this.bgColor = '#f0f8ff';
+    this.boxShadow = '0 4px 12px rgba(0,0,0,0.2)';
   }
 
   @HostListener('mouseleave') onMouseLeave() {
-    this.renderer.setStyle(this.el.nativeElement, 'transform', 'scale(1)');
-    this.renderer.setStyle(this.el.nativeElement, 'backgroundColor', 'white');
-    this.renderer.removeStyle(this.el.nativeElement, 'boxShadow');
+    // this.renderer.setStyle(this.el.nativeElement, 'transform', 'scale(1)');
+    // this.renderer.setStyle(this.el.nativeElement, 'backgroundColor', 'white');
+    // this.renderer.removeStyle(this.el.nativeElement, 'boxShadow');
+
+    this.transform = 'scale(1)';
+    this.bgColor = 'white';
+    this.boxShadow = '';
   }
 
 }
